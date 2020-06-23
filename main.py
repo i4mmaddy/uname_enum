7d86184b1a53e073a2d20257f4060e6869dff7f8
Madhavan Mani <maadhu97.madhavan@gmail.com>
2020-06-23 10:22:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("hello world made a commit after cron")
+// to check the given usernames present on different socialmedia
+
+//instagram 
+
+
+
+Insta_url  = 'https://instagram.com/'
+
+
+fb_url = 'https://facebook.com/'
+
+
+//to get the given username url or read usenames from the file
+
+
+type = sysargv(1)
+
+if(type == 1)
+  uname = sysargv(2)
+else(type == 2)
+  
+  // needs to be reterive from file
+  
+  
+  
+  
+
+
